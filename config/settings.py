b60a16c9c879169ea5c5aa3779bf6ee7f9e5f3c0
keynneravanser <keynneravanser@gmail.com>
2026-02-10 16:24:14 -0500

    Actualizacion de cors

--- config/settings.py
+++ config/settings.py
@@ -151,7 +151,38 @@ CSRF_TRUSTED_ORIGINS = [
     if o.strip()
 ]
 
+# =========================
+# CORS (FRONTEND)
+# =========================
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://campesena-backend.onrender.com",
+]
 # =========================
 # Cloudinary
 # =========================
